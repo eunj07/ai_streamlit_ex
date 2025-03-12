c5b35403704cbefbb36f4695102db5a7c96edd85
eunj07 <eun1234501@gmail.com>
2025-03-12 12:04:21 +0900

    Streamlit_test_code

---
+++ 1.st_test.py
@@ -0,0 +1,6 @@
+# streamlit 실행방법: streamlit run 1.st_test.py
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.title('Uber pickups in NYC')
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # ai_streamlit_ex
 Streamlit
+openai의 Streamlit을 활용한 서비스 개발
+
+# 설치 라이브러리
+```
+pip install openai
+pip install streamlit
+```
